8a56591a0db264de52c44376027ab36f28fdec28
uchicagofengyufei <zjufengyufei@gmail.com>
2017-06-16 21:48:31 -0500

    customized tensor operator sdot
    The theano tensor operator customized for asymmetric backpropagation

--- tensor_op.py
+++ tensor_op.py
@@ -2,7 +2,16 @@ from theano import gof
 import theano.tensor as T
 import numpy
 
+# customized theano tensor operation, for asymmetric backpropagation
+# Things are simple
+# When we compute the gradient in the auto differentiation graph
+# when computing gradient for previous hidden state, we need a dot involving gradient and the weight
+# Now substitute the weight with something else, possibly random, and we can update the random feedback the same as the weight
+# biological neural network is crazy things indeed
 
+
+# This is customized API that theano provided.
+# everything except the gradient is the same as theano.tensor.dot
 class SDotOp(gof.Op):
 
     __props__ = ("name", "fn")
@@ -16,6 +25,8 @@ class SDotOp(gof.Op):
         return gof.Apply(self, [x, y,sub_mat], [T.fmatrix()])
 
     def perform(self, node, inp, out):
+        # three things involved: X,Y, random feedback sub_mat
+        # sub_mat do not goes into the forward path
         x, y,sub_mat = inp
         z, = out
         z[0] = self.fn(x,y)
@@ -27,8 +38,11 @@ class SDotOp(gof.Op):
         x, y, sub_mat = inp
         gz, = grads
 
+        # gradient in the backpropagation is substituted here
         x_grad = T.dot(gz,sub_mat.T)
         y_grad = T.dot(x.T, gz)
+        
+        # update the random feedback, or not(force gradient to be 0 for random feedback)
         #sub_grad = y_grad*0
         sub_grad = y_grad
 
@@ -36,7 +50,7 @@ class SDotOp(gof.Op):
 
         return rval
 
-
+# optional algorithm, will be automatically changed into GPU operation
 s_dot = SDotOp(name='SDot',
                      fn=lambda x, y: numpy.dot(x,y))
 
